beb606b8cd25b59cc87f9ad804cdaaaadcd3d4dd
Yash Chaurasia <53575788+YashChaurasia07@users.noreply.github.com>
2023-07-31 14:32:50 +0530

    Time: 0 ms (100%), Space: 6.6 MB (95.66%) - CodeSyncProV2

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,58 +1,29 @@
-// class Solution {
-// public:
-//     string removeOuterParentheses(string s) {
-//         string res = "";
-//         int j =0;
-//         // for(int i = 0; i<s.size();i++)
-//         // {
-//         //     if(s[i] == 41)
-//         //     {
-//         //         j--;
-//         //     }
-//         //     if(s[i] == 40)
-//         //     {
-//         //         j++;
-//         //     }
-//         //     if(j != 0)
-//         //     {
-//         //         res += s[i]; 
-//         //     }
-//         // }
-//         return res;
-//     }
-// };
-
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        // to store the ans
         string ans = "";
-        // to keep the track of valid parenthesis we have initialized count variable
-        // valid parenthesis contains equal number of 
-        int count = 0;
-        for(char ch: s){
-            if(ch == '(' && count == 0){
-                // this bracket is a part of outermost parenthesis
-                // dont add it to the ans, skip it
-                count++;
-            }else if(ch == '(' && count >= 1){
-                // this bracket is a part of inner parenthesis
-                // add it to the ans
-                ans += ch;
-                count++;
-                
-            }else if(ch == ')' && count > 1){
-                // this bracket is a part of inner parenthesis
-                // add it to the ans, and decrease the count
-                ans += ch;
-                count--;
-            }else if(ch == ')' && count == 1){
-                // this bracket is a part of outer parenthesis
-                // avoid it
-                count--;
+        int cnt = 0;
+        for(char c: s)
+        {
+            if( c == '(' && cnt == 0)
+            {
+                cnt++;
+            }
+            else if(c=='(' && cnt >= 1)
+            {
+                ans += c;
+                cnt++;
+            }
+            else if(c == ')' && cnt >1)
+            {
+                ans+=c;
+                cnt--;
+            }
+            else 
+            {
+                cnt--;
             }
         }
-        
         return ans;
     }
 };
