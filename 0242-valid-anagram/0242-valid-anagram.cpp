469007f9671a1bad75ace7ed0f04956e75a04948
Yash Chaurasia <53575788+YashChaurasia07@users.noreply.github.com>
2023-07-31 17:57:32 +0530

    Time: 7 ms (80.87%), Space: 7.3 MB (79.34%) - CodeSyncProV2

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,11 +5,19 @@ public:
         {
             return false;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i = 0;i<s.length();i++)
+        int freq[26] = {0};
+
+        for(int i = 0; i<s.length();i++)
         {
-            if(s[i]!=t[i])
+            freq[s[i]- 'a']++;    
+        }
+        for(int i = 0; i<t.length();i++)
+        {
+            freq[t[i] - 'a']--;    
+        }
+        for(int i = 0;i<26;i++)
+        {
+            if(freq[i]!=0)
             {
                 return false;
             }
