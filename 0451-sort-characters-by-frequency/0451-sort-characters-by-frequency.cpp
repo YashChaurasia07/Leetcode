f740238b1fc96d4419dcf52b8a73feaac736a6b1
Yash Chaurasia <53575788+YashChaurasia07@users.noreply.github.com>
2023-08-01 10:44:33 +0530

    Time: 6 ms (97.34%), Space: 8.4 MB (45.09%) - CodeSyncProV2

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char,int> m;
+        map<char,int> m;
         for(int i = 0; i<s.length();i++)
         {
             m[s[i]]++;
         }
         string s1 ="";
-        vector<pair<char,int>>v(m.begin(),m.end());
-        sort(v.begin(), v.end(), [](const pair<char,int>& a, const pair<char, int>& b) {
-            return a.second > b.second;
-        });
-        for(auto x : v)
+        
+        priority_queue<pair<int,char>> q;
+        for(auto it : m)
+            q.push({it.second,it.first});
+
+        
+        while(!q.empty())
         {
-            for(int i = 0; i<x.second;i++)
+            for(int i = 0; i<q.top().first;i++)
             {
-                s1+=x.first;
+                s1+=q.top().second;
             }
+            q.pop();
         }
         return s1;
     }
