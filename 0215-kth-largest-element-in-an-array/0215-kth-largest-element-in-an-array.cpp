7e37b4f174b95f3e44dce5f6443c58610469b51e
Yash Chaurasia <53575788+YashChaurasia07@users.noreply.github.com>
2023-08-08 15:55:13 +0530

    Time: 90 ms (88.78%), Space: 47.3 MB (32.71%) - CodeSyncProV2

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> h;
+        for(int i = 0;i<nums.size();i++){
+            h.push(nums[i]);
+            if(h.size()>k)
+                h.pop();
+        }
+        return h.top();
+    }
+};
