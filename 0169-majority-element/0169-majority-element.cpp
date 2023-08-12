25e280693dffb6ce0913db0a3d713d47c85617e0
Yash Chaurasia <53575788+YashChaurasia07@users.noreply.github.com>
2023-08-12 16:53:52 +0530

    Time: 21 ms (52.19%), Space: 19.6 MB (56.62%) - CodeSyncProV2

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> m;
-        int n = nums.size()-1;
-        for(auto it:nums) m[it]++;
-
-        for(auto it : m) 
-        {
-            if(it.second > n/2) return it.first;
-        }
-        return 0;
+      sort(nums.begin(),nums.end());
+      int n = (nums.size()-1)/2;
+      return nums[n];
     }
 };
