049b94e11d02abf382f14d9611b4e91a6461b357
Yash Chaurasia <53575788+YashChaurasia07@users.noreply.github.com>
2023-08-17 16:13:24 +0530

    Time: 50 ms (61.49%), Space: 159.7 MB (22.93%) - CodeSyncProV2

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int check(string s, int p1,int p2,int &st,int &en)
+    {
+        int count = 0;
+        if(p1==p2)
+        {
+            count++;
+            p1--;
+            p2++;
+        }
+        while(p1>=0 and p2<s.length())
+        {
+            if(s[p1] != s[p2])
+            {
+                st=p1+1;
+                en=p2-1;
+                return count;
+            }
+            else
+            {
+                p1--;
+                p2++;
+                count += 2;
+            }
+        }    
+        st=p1+1;
+        en=p2-1;
+        return count; 
+    }
+    string longestPalindrome(string s) {
+        int st = -1;
+        int en = -1;
+        int len = 0;
+        int fs = -1;
+        int fe = -1;
+        int n = s.length();
+        for(int i = 0;i<n;i++)
+        {
+            int x = check(s,i,i,st,en);
+            if(x>len)
+            {
+                len = x;
+                fs = st;
+                fe = en;
+            }
+            int y = check(s,i,i+1,st,en);
+            if(y>len)
+            {
+                len = y;
+                fs = st;
+                fe = en;
+            }
+        }
+        return s.substr(fs,fe-fs+1);
+    }
+};
